1de7ab6396c481e40afdc65c485b44d5084134a0
henry.lee <lhh@lineavn.com>
2021-08-08 22:25:49 +0700

    User: CRUD for user

--- src/main/java/com/cm/core/CoreApplication.java
+++ src/main/java/com/cm/core/CoreApplication.java
@@ -1,13 +1,23 @@
 package com.cm.core;
 
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer customizer()
+	{
+		return builder -> builder.serializerByType(ObjectId.class,new ToStringSerializer());
 	}
 
 }
---
+++ src/main/java/com/cm/core/service/UserService.java
@@ -0,0 +1,56 @@
+package com.cm.core.service;
+
+import com.cm.core.entity.User;
+import com.cm.core.enumeration.UserRole;
+import com.cm.core.repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        validateUser(user);
+
+        return userRepository.insert(user);
+    }
+
+    public User updateUser(ObjectId userId, User updatingUser) {
+        validateUserExisting(userId);
+//        validateUser(updatingUser);
+
+        updatingUser.setId(userId);
+
+        return userRepository.save(updatingUser);
+    }
+
+    public void deleteUser(ObjectId userId) {
+        Optional<User> existingUser = userRepository.findById(userId);
+
+        if (!existingUser.isPresent())
+            throw new IllegalStateException("User Id: " + userId.toHexString() + " not found.");
+        else userRepository.delete(existingUser.get());
+    }
+
+    public void validateUser(User user) {
+        Optional<User> existingUserOpt = userRepository.findByEmail(user.getEmail());
+        if (existingUserOpt.isPresent())
+            throw new IllegalStateException("Email " + user.getEmail() + " has taken!");
+    }
+
+    public void validateUserExisting(ObjectId userId) {
+        Optional<User> existingUser = userRepository.findById(userId);
+        if (!existingUser.isPresent())
+            throw new IllegalStateException("User Id: " + userId.toHexString() + " not found.");
+    }
+}
