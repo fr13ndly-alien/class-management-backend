63574e09bce2d705cc73452d53df6f5525f4d59c
henry.lee <lhh@lineavn.com>
2021-11-02 14:46:46 +0700

    Denormalize teacher information into group

--- src/main/java/com/cm/core/CoreApplication.java
+++ src/main/java/com/cm/core/CoreApplication.java
@@ -6,14 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
--- src/main/java/com/cm/core/service/GroupService.java
+++ src/main/java/com/cm/core/service/GroupService.java
@@ -1,6 +1,7 @@
 package com.cm.core.service;
 
 import com.cm.core.entity.Group;
+import com.cm.core.entity.UserRef;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class GroupService {
     @Autowired
     MongoTemplate mongoTemplate;
 
+    @Autowired
+    UserService userService;
+
     private final String collectionName = "group";
     private final Class<Document> groupEntity = Document.class;
 
@@ -32,7 +36,16 @@ public class GroupService {
                 throw new IllegalStateException("Missing field: " + field);
         }
 
-        // todo: extract teacher.id to ObjectId
+        String teacherStrId = groupDoc.getString("teacher");
+        Document foundTeacher = userService.findById(new ObjectId(teacherStrId));
+        groupDoc.replace(
+            "teacher",
+            new UserRef(
+                foundTeacher.getObjectId("_id").toHexString(),
+                foundTeacher.getString("name")
+            )
+        );
+
         groupDoc.append("createdDate", new Date());
         groupDoc.append("lastModified", new Date());
         groupDoc.append("_id", new ObjectId());
